6bea2bfe14c713876069f6c594c0b249b3d94d99
Asteinwords <aman11202004@gmail.com>
2025-08-15 02:19:04 +0530

    Blunder

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const socketUserMap = new Map();
 // Define allowed origins
 const allowedOrigins = [
   process.env.CLIENT_URL || 'http://localhost:5173',
-  'https://astonishing-genie-c725b3.netlify.app/',
+  'https://astonishing-genie-c725b3.netlify.app',
 ];
 
 const corsOptions = {
